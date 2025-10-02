a3db9a56317517d234d53e97cd3ba6e451c1b351
James Edward Gray II <james@graysoftinc.com>
2025-10-02 14:14:10 -0500

    Chapter 5.

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 import Config
+config :tunez, token_signing_secret: "1d0y/ZZp3FObeHBys78yP4IIe2AGcLC+"
+config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true], disable_async?: true
 
 # Configure your database
